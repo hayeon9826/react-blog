a5d825fdc81fbd380ff19d48cfb7c0dd4c3b055e
hayeon9826 <hayeon226@likelion.org>
2023-05-28 02:07:23 +0900

    feat: 1-3. home 컴포넌트 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 
+import { AiFillHeart } from "react-icons/ai";
+
 export default function Home() {
   return (
     <div>
@@ -11,11 +13,38 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="Post-list">Post List</div>
+      <div className="Post__navigation">
+        <div className="Post__navigation-active">추천</div>
+        <div>팔로잉</div>
+      </div>
+      <div className="Post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="Post__box">
+            <Link to={`/posts/${e}`}>
+              <div className="Post__profile-box">
+                <div className="Post__profile" />
+                <div className="Post__author-name">Adarsh Dayanand</div>
+                <div className="Post__date">Apr 12</div>
+              </div>
+              <h1 className="Post__title">Microservices Architecture with Node.js: Building Scalable and Robust Applications</h1>
+              <div className="Post__text">
+                Microservices architecture has gained popularity in recent years as a modern approach to building large-scale, complex applications. …
+              </div>
+              <div className="Post__utils-box">
+                <div className="Post__delete">삭제</div>
+                <div className="Post__edit">수정</div>
+                <div className="Post__save">
+                  <AiFillHeart />
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>Menu 1</div>
-        <div>Menu 2</div>
-        <div>Menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   );
